e21c7148926144e9d9689903eda98e5e5b5e1944
Héctor <geco.hh@gmail.com>
2014-10-13 19:50:20 +0100

    starter.sh modificado y añadido a ignore

--- starter.sh
+++ starter.sh
@@ -1,5 +1,5 @@
 #ª/bin/bash
-if []
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then 
 	export PATH=/usr/local/bin:$PATH
 	forever start --sourceDir /opt/medusa/api server.js
