b2d09571314e05c3d71553fe83e3859091411d69
Héctor <geco.hh@gmail.com>
2014-10-28 12:53:36 +0000

    cambios de estetica en client controller

--- app/controllers/client.server.controller.js
+++ app/controllers/client.server.controller.js
@@ -3,42 +3,51 @@
 var mongoose = require('mongoose');
 
 exports.requestTicket = function(req, res){
-	var params = {"phoneId" : req.param('phoneId')};
-	return res.status(400).send(params);
-};
-exports.requestMatch = function(req, res){
-	var params = {"ticketId" : req.param('ticketId')};
-	return res.status(400).send(params);
-};
-exports.waitTurn = function(req, res){
-	var params = {	"matchId" : req.param('matchId'),
-					"ticketId": req.param('ticketId'),
-					"turnId": req.param('turnId')
-				};
-	return res.status(400).send(params);
-
-};
-exports.submitTurn = function(req, res){
-	var params = {	"matchId": req.param('matchId'),
-					"ticketId": req.param('ticketId'),
-					"turnId": req.param('turnId'),
-					"turnInfo": req.param('turnInfo')
+	var params = {
+		"phoneId" : req.param('phoneId')
 	};
 	return res.status(400).send(params);
-
 };
-exports.getMatchStatus = function(req, res){
-	var params = {	"matchId" : req.param('matchId'),
-				"ticketId": req.param('ticketId')
-			};
+
+exports.requestMatch = function(req, res){
+	var params = {
+		"ticketId" : req.param('ticketId')
+	};
 	return res.status(400).send(params);
-
-
 };
+
+exports.waitTurn = function(req, res){
+	var params = {
+		"matchId" : req.param('matchId'),
+		"ticketId": req.param('ticketId'),
+		"turnId": req.param('turnId')
+	};
+	return res.status(400).send(params);
+};
+
+exports.submitTurn = function(req, res){
+	var params = {	
+		"matchId": req.param('matchId'),
+		"ticketId": req.param('ticketId'),
+		"turnId": req.param('turnId'),
+		"turnInfo": req.param('turnInfo')
+	};
+	return res.status(400).send(params);
+};
+
+exports.getMatchStatus = function(req, res){
+	var params = {	
+		"matchId" : req.param('matchId'),
+		"ticketId": req.param('ticketId')
+	};
+	return res.status(400).send(params);
+};
+
 exports.setMatchStatus = function(req, res){
-	var params = {	"matchId" : req.param('matchId'),
-					"ticketId": req.param('ticketId'),
-					"statusInfo": req.param('statusInfo')
-				};
+	var params = {	
+		"matchId" : req.param('matchId'),
+		"ticketId": req.param('ticketId'),
+		"statusInfo": req.param('statusInfo')
+	};
 	return res.status(400).send(params);
 };
