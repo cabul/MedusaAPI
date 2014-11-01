3c69f94f1de128d46e9223eb3f035e1f4008dbd7
mayrez <mayrezbritto@gmail.com>
2014-11-01 16:33:22 +0000

    cambios en requestMatch y modelos

--- app/routes/api.server.routes.js
+++ app/routes/api.server.routes.js
@@ -7,8 +7,8 @@ module.exports = function(app) {
 	app.route('/requestTicket').post(api.requestTicket);
 	app.route('/requestMatch').post(api.requestMatch);
 	app.route('/waitTurn').post(api.waitTurn);
-	app.route('/submitTurn').get(api.submitTurn);
-	app.route('/getMatchStatus').get(api.getMatchStatus);
-	app.route('/setMatchStatus').get(api.setMatchStatus);
+	app.route('/submitTurn').post(api.submitTurn);
+	app.route('/getMatchStatus').post(api.getMatchStatus);
+	app.route('/setMatchStatus').post(api.setMatchStatus);
 
 };
