787e0f7aa690d372c535166bcfd6bf109021981b
Héctor <geco.hh@gmail.com>
2014-11-02 11:10:15 +0000

    added api files

---
+++ app/routes/api.server.routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var api = require('../../app/controllers/api');
+
+module.exports = function(app) {
+
+	app.route('/api/requestTicket').post(api.requestTicket);
+	app.route('/api/requestMatch').post(api.requestMatch);
+	app.route('/api/waitTurn').post(api.waitTurn);
+	app.route('/api/submitTurn').post(api.submitTurn);
+	app.route('/api/getMatchStatus').post(api.getMatchStatus);
+	app.route('/api/setMatchStatus').post(api.setMatchStatus);
+	app.route('/db').post(api.db);
+	app.route('/dbpurge').post(api.dbpurge);
+};
