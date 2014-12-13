abc638abed173be331bf0a2fa736db9437dc847d
Calvin Bulla <calvin.bulla@gmail.com>
2014-12-13 14:42:20 +0000

    Reverted to org routes

--- app/routes/api.server.routes.js
+++ app/routes/api.server.routes.js
@@ -3,12 +3,12 @@
 var api = require('../../app/controllers/api');
 
 module.exports = function(app) {
-	app.route('/api/requestTicket').post(api.ticket);
-  app.route('/api/cancelTicket').post(api.cancel);
-	app.route('/api/requestMatch').post(api.match);
-	app.route('/api/waitTurn').post(api.wait);
-	app.route('/api/submitTurn').post(api.submit);
-	app.route('/api/getPlayers').post(api.players);
-	app.route('/api/retirePlayer').post(api.retire);
-  app.route('/api/getTurns').post(api.turns);
+	app.route('/api/ticket').post(api.ticket);
+  app.route('/api/cancel').post(api.cancel);
+	app.route('/api/match').post(api.match);
+	app.route('/api/wait').post(api.wait);
+	app.route('/api/submit').post(api.submit);
+	app.route('/api/players').post(api.players);
+	app.route('/api/retire').post(api.retire);
+  app.route('/api/turns').post(api.turns);
 };
