119b3018e4e0c72b43f8bc2b97d20f5cfb4efd02
Calvin Bulla <calvin.bulla@gmail.com>
2014-12-17 13:59:35 +0000

    Changed db routes to GET INSECURE

--- app/routes/db.server.routes.js
+++ app/routes/db.server.routes.js
@@ -4,7 +4,7 @@ var db = require('../../app/controllers/db');
 
 module.exports = function(app) {
 
-	app.route('/db/display').post(db.dbdisplay);
-	app.route('/db/purge').post(db.dbpurge);
+	app.route('/db/display').get(db.dbdisplay);
+	app.route('/db/purge').get(db.dbpurge);
 
 };
